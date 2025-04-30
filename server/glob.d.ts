ee1efce4696297fa8e412d236476ca270cb345a5
ljci <liujiachi26@gmail.com>
2025-04-30 14:50:33 +0800

    fixing: reddit

--- server/glob.d.ts
+++ server/glob.d.ts
@@ -25,6 +25,7 @@ declare module 'glob:./sources/{*.ts,**/index.ts}' {
   export const nowcoder: typeof import('./sources/nowcoder')
   export const pcbeta: typeof import('./sources/pcbeta')
   export const producthunt: typeof import('./sources/producthunt')
+  export const reddits: typeof import('./sources/reddits')
   export const smzdm: typeof import('./sources/smzdm')
   export const solidot: typeof import('./sources/solidot')
   export const sputniknewscn: typeof import('./sources/sputniknewscn')
